71fecaea4488826c0ccf1c3459a8798fa451a031
gaheul <rkgmf1230@naver.com>
2022-07-01 15:42:47 +0900

    make it better

--- src/content/service.jsx
+++ src/content/service.jsx
@@ -19,32 +19,32 @@ function AddService(){
     const serviceList = [
         {
             img:'https://contents-image.twayair.com/homepage/images/main/img_srv01.png',
-            name:'부가서비스 번들'
+            name:'부가서비스\n번들'
         },
         {
             img:'	https://contents-image.twayair.com/homepage/images/main/img_srv02.png',
-            name:'수하물 추가구매'
+            name:'수하물 추가\n구매'
         },
         {
             img:'	https://contents-image.twayair.com/homepage/images/main/img_srv03.png',
-            name:'사전 좌석 구매'
+            name:'사전 좌석\n구매'
         },
         {
             img:'https://contents-image.twayair.com/homepage/images/main/img_srv04.png',
-            name:'기내식 사전주문'
+            name:'기내식 사전\n주문'
         },
         {
             img:'https://contents-image.twayair.com/homepage/images/main/img_srv05.png',
-            name:`면세품 & t'shop`
+            name:`면세품 &\n t'shop`
         },
         {
             img:'https://contents-image.twayair.com/homepage/images/main/img_srv07.png',
-            name:'원클릭 여행 보험'
+            name:'원클릭\n여행 보험'
         }
     ]
 
     return(
-        <div style={{display:'flex'}}>
+        <div className={styles.content_item}>
             {serviceList.map(add=>(
                 <Service add={add} key={add.name}/>
             ))}
